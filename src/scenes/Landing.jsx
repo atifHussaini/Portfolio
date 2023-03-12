8b3772e584a7a28783f8a1fc89ac038b0882d0c4
Atif Hussaini <114316821+AtifHussaini@users.noreply.github.com>
2023-03-12 13:00:30 -0500

    Last name brush fix

--- src/scenes/Landing.jsx
+++ src/scenes/Landing.jsx
@@ -15,9 +15,25 @@ const Landing = ({setSelectedPage}) => {
                         <img alt="profile" src="assets/profile-image.png" className="hover:filter hover:saturate-200 transition duration-500 z-10 w-full max-w-[400px] md:max-w-[600px]"/>
                     </div>
                 ) : (
-                    <div> </div>
+                    <img alt="profile" src="assets/profile-image.png" className="hover:filter hover:saturate-200 transition duration-500 z-10 w-full max-w-[400px] md:max-w-[600px]"/>
                 )}
             </div>
+                {/* MAIN SECTION */} 
+                <div className="z-30 basis-2/5 mt-12 md:mt-32">
+                    {/* HEADINGS */}
+                    <motion.div initial="hidden" whileInView="visible" viewport= {{ once: true, amount: 0.5}} transition={{ duration: 0.5 }} variants={{ hidden: {opacity: 0, x: -50}, visible: 
+                { opacity: 1, x: 0}}}>
+                        <p className="text-6xl font-playfair z-10 text-center md:text-start">
+                            Atif {" "}
+                            <span className="xs: relative xs:text-deep-blue xs: font-semibold z-20 xs:before:content-brush before:absolute before:-left-[35px] before:-top-[120px] before:z-[-1]">
+                                Hussaini
+                            </span>
+                        </p>
+                        <p className="mt-10 mb-7 text-sm text:center md:text-start">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mattis molestie a iaculis at. 
+                        </p>
+                    </motion.div>
+                </div>
         </section>
     );
 }
