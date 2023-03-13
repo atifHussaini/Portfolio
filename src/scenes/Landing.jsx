e39c3cd56966db787115a4d9a9ab741efb5e60fc
Atif Hussaini <114316821+AtifHussaini@users.noreply.github.com>
2023-03-13 12:00:39 -0500

    Fixed profile pic

--- src/scenes/Landing.jsx
+++ src/scenes/Landing.jsx
@@ -11,8 +11,8 @@ const Landing = ({setSelectedPage}) => {
             {/* IMAGE SECTION */}
             <div className="md:order-2 flex justify-center basis-3/5 z-10 mt-16 md:mt-32">
                 {isAboveMediumScreens ? (
-                    <div className="relative z-0 ml-20 before:absolute before:-top-20 before:-left-20 before:rounded-t-[400px] before:w-full before:max-w-[400px] before:h-full before:border-2 before:border-blue before:z-[-1] ">
-                        <img alt="profile" src="assets/profile-image.png" className="hover:filter hover:saturate-200 transition duration-500 z-10 w-full max-w-[400px] md:max-w-[600px]"/>
+                    <div className="relative z-0 ml-20 before:absolute before:-top-20 before:-left-20 before:w-full before:max-w-[600px] before:h-full before:border-2 before:border-blue before:z-[-1] ">
+                        <img alt="profile" src="assets/profile-image.png" className="hover:filter hover:saturate-200 transition duration-500 z-10 w-full max-w-[400px] md:max-w-[600px] border-radius:1rem"/>
                     </div>
                 ) : (
                     <img alt="profile" src="assets/profile-image.png" className="hover:filter hover:saturate-200 transition duration-500 z-10 w-full max-w-[400px] md:max-w-[600px]"/>
