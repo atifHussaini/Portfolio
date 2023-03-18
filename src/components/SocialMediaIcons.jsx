0a3c640c9fd3036e5a880c38454477ce42cf7572
Atif Hussaini <114316821+AtifHussaini@users.noreply.github.com>
2023-03-18 13:48:11 -0400

    Social media icons done

---
+++ src/components/SocialMediaIcons.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const SocialMediaIcons = () => {
+    return (
+        <div className="flex justify-center md:justify-start my-10 gap-7">
+            <a className="hover:opacity-50 transition duration-500" href="https://linkedin.com/in/atif-hussaini/" target="_blank" rel="noreferrer">
+                <img alt="linkedin-link" src="../assets/linkedin.png"/>
+            </a>
+            <a className="hover:opacity-50 transition duration-500" href="https://twitter.com/2weekatif" target="_blank" rel="noreferrer">
+                <img alt="twitter-link" src="../assets/twitter.png"/>
+            </a>
+            <a className="hover:opacity-50 transition duration-500" href="https://facebook.com/crazy1ndn" target="_blank" rel="noreferrer">
+                <img alt="facebook-link" src="../assets/facebook.png"/>
+            </a>
+            <a className="hover:opacity-50 transition duration-500" href="https://instagram.com/atifhussaini_" target="_blank" rel="noreferrer">
+                <img alt="instagram-link" src="../assets/instagram.png"/>
+            </a>
+            <a className="hover:opacity-50 transition duration-500" href="https://github.com/atifhussaini" target="_blank" rel="noreferrer">
+                <img alt="github-link" src="../assets/github.png" className="h-8"/>
+            </a>
+        </div>
+    );
+}
+
+export default SocialMediaIcons;
