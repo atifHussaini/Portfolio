555084f0b50bd1825eee9c8fe7c0c0e62ce5cc47
Atif Hussaini <114316821+AtifHussaini@users.noreply.github.com>
2023-03-20 12:20:06 -0400

    projects grid

--- src/scenes/Projects.jsx
+++ src/scenes/Projects.jsx
@@ -15,7 +15,7 @@ const projectVariant = {
 
 const Project = ({title, subtitle}) => {
     const overlayStyles = `absolute h-full w-full opacity-0 hover:opacity-90 transition duration-500 bg-grey z-30 flex flex-col justify-center items-center text-center p-16 text-deep-blue`
-    const projectTitle = title.split(" ").join("-").toLowerCase();
+    const projectTitle = title.toLowerCase().split(" ").join("-");
 
     return (
         <motion.div variants={projectVariant} className="relative">
