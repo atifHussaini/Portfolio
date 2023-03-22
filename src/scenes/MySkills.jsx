0156cb0312701275a91135c6c5571690190dab95
Atif Hussaini <114316821+AtifHussaini@users.noreply.github.com>
2023-03-22 13:42:58 -0400

    skills changes

--- src/scenes/MySkills.jsx
+++ src/scenes/MySkills.jsx
@@ -113,7 +113,7 @@ const MySkills = (props) => {
             <div className="z-10">
               <p className="font-playfair font-semibold text-5xl">03</p>
               <p className="font-playfair font-semibold text-3xl mt-3">
-                HTML5/CSS3
+                HTML/CSS
               </p>
             </div>
             <div className="w-1/2 md:w-3/4 h-32 bg-yellow absolute right-0 top-0 z-[-1]" />
